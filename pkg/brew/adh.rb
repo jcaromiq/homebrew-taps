fe1e826a5917fcd6482b43193a76d137c58254a6
Joaquin Caro <jcaromiq@gmail.com>
2021-02-28 23:49:05 +0100

    version

--- pkg/brew/adh.rb
+++ pkg/brew/adh.rb
@@ -1,11 +1,11 @@
 class Adh < Formula
-  version '1.1.2'
+  version '1.1.3'
   desc "A port from https://github.com/ApiumhubOpenSource/adh in rust"
   homepage "https://github.com/jcaromiq/adh-rust"
 
   if OS.mac?
       url "https://github.com/jcaromiq/adh-rust/releases/download/#{version}/adh-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "4575b0919dff4cabff146e42c6ddc37dc6afe78d41a8d993f4a7d08e834f41cd"
+      sha256 "b35f2dd242ef852f2c57c8fd73d7d76eaf116142c92b67d94fc5b7b14a788c2a"
   end
 
   def install
